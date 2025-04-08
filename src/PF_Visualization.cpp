93dd9d7ec9dabc6e9bb2338f442505773d19eb94
GUCCIZUMI <gutiyosizumi@gmail.com>
2025-04-08 22:23:35 +0900

    aba

--- src/PF_Visualization.cpp
+++ src/PF_Visualization.cpp
@@ -118,7 +118,7 @@ void PFVisualization::localization()
     
         for (const auto& marker_id:in_range_ids)
         {
-            getLikelihood_main(marker_id);
+            getLikelihood(marker_id);
         }
     
         normLiklihood();
